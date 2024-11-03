ec27c94d6a756f91bfb63a9e95a7912cf58d2d68
AlexanderB <cr1zp@live.com>
2024-11-03 17:34:05 +0100

    Update
    Added 2 Victory sounds
    Added 2 Defeat sounds
    Updated the toc so you can load the sounds through /dbm
    Created a Lua script that defines the sound files

---
+++ DBM-VPForsen/LoadList.lua
@@ -0,0 +1,33 @@
+local insert = table.insert
+local inserted1, inserted2 = false, false
+
+
+function DBMVPForsen() -- Register Victory sounds to DBM.Victory table
+    if inserted1 then
+        return
+    end
+    insert(DBM.Victory, {
+        text    = "Forsen: Alright",
+        value   = "Interface\\AddOns\\DBM-VPForsen\\Victory\\ForsenVictory1.ogg"
+    })
+    insert(DBM.Victory, {
+        text    = "Forsen: 15:28",
+        value   = "Interface\\AddOns\\DBM-VPForsen\\Victory\\ForsenVictory2.ogg"
+    })
+    inserted1 = true
+end
+
+function DBMDPForsen() -- Register Defeat sounds to DBM.Victory table
+    if inserted2 then
+        return
+    end
+    insert(DBM.Defeat, {
+        text    = "Forsen: Children's game",
+        value   = "Interface\\AddOns\\DBM-VPForsen\\Defeat\\ForsenDefeat1.ogg"
+    })
+    insert(DBM.Defeat, {
+        text    = "Forsen: Legolas",
+        value   = "Interface\\AddOns\\DBM-VPForsen\\Defeat\\ForsenDefeat2.ogg"
+    })
+    inserted2 = true
+end
