ae1accf0d3cf34f54310ee4bb5a76cae0220052d
AlexanderB <cr1zp@live.com>
2024-11-03 19:53:19 +0100

    Update
    forsen can now count to 10 in bigwigs

---
+++ BigWigs_Voice_Forsen/BigWigs_Voice_Forsen.lua
@@ -0,0 +1,23 @@
+local heroes = {
+	"Fors",
+}
+local key = "Forsen"
+local path = "Interface\\AddOns\\BigWigs_Voice_Forsen\\sound\\%s_%d.ogg"
+
+
+for i = 1, #heroes do
+	local hero = heroes[i]
+	BigWigsAPI:RegisterCountdown(key:format(hero), {
+		path:format(hero, 1), 
+		path:format(hero, 2),
+		path:format(hero, 3),
+		path:format(hero, 4),
+		path:format(hero, 5),
+		path:format(hero, 6),
+		path:format(hero, 7),
+		path:format(hero, 8),
+		path:format(hero, 9),
+		path:format(hero, 10),
+	})
+end
+
